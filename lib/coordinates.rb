c4bec3e6c6f9ba5a8db046562a4b23c0e317c67c
Charles Hoffman <chuck@hoff2.com>
2018-10-18 17:32:17 -0500

    rubocop suggests freezing constants to make them... constant

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -1,13 +1,13 @@
 module WordSearch
   module Coordinates
-    HORIZONTAL = [1, 0]
-    VERTICAL = [0, 1]
-    DESCENDING = [1, 1]
-    ASCENDING = [1, -1]
-    REVERSE_HORIZONTAL = [-1, 0]
-    REVERSE_VERTICAL = [0, -1]
-    REVERSE_DESCENDING = [-1, -1]
-    REVERSE_ASCENDING = [-1, 1]
+    HORIZONTAL = [1, 0].freeze
+    VERTICAL = [0, 1].freeze
+    DESCENDING = [1, 1].freeze
+    ASCENDING = [1, -1].freeze
+    REVERSE_HORIZONTAL = [-1, 0].freeze
+    REVERSE_VERTICAL = [0, -1].freeze
+    REVERSE_DESCENDING = [-1, -1].freeze
+    REVERSE_ASCENDING = [-1, 1].freeze
 
     def coordinate_list(start_column, start_row, direction, length)
       if length == 0
