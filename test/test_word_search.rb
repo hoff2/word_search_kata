f7a35d0fab2796077711b93e829d05b43eb27c01
Charles Hoffman <chuck@hoff2.com>
2018-10-18 16:10:53 -0500

    move test grid initialization to setup

--- test/test_word_search.rb
+++ test/test_word_search.rb
@@ -4,8 +4,8 @@ require 'minitest/autorun'
 class WordSearchTest < Minitest::Test
   include WordSearch
 
-  def test_initializing_grid
-    Grid.new([
+  def setup
+    @grid = Grid.new([
       %w( A B C D E ),
       %w( F G H I J ),
       %w( K L M N O ),
@@ -15,13 +15,6 @@ class WordSearchTest < Minitest::Test
   end
 
   def test_indexing_grid_by_column_and_row
-    grid = Grid.new([
-      %w( A B C D E ),
-      %w( F G H I J ),
-      %w( K L M N O ),
-      %w( P Q R S T ),
-      %w( U V W X Y )
-    ])
-    assert_equal('U', grid.letter_at(0, 4))
+    assert_equal('U', @grid.letter_at(0, 4))
   end
 end
