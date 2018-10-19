19bb3f8efb7b3a4de3cd1caad60bf4793bdbc8e6
Charles Hoffman <chuck@hoff2.com>
2018-10-19 13:24:55 -0500

    actually we need something more like this
    because we want to save the coordinate list for the output

--- lib/search.rb
+++ lib/search.rb
@@ -16,5 +16,12 @@ module WordSearch
         @grid.letter_match_at?(letter, coordinate[0], coordinate[1])
       end
     end
+
+    def word_matches_coordinate_list?(word, coordinates)
+      checks = coordinates.zip(word.split(''))
+      checks.all? do |coordinate, letter|
+        @grid.letter_match_at?(letter, coordinate[0], coordinate[1])
+      end
+    end
   end
 end
--- test/test_search.rb
+++ test/test_search.rb
@@ -26,4 +26,20 @@ class TestSearch < MiniTest::Test
     refute(@search.word_at_position_in_direction?(
       "WORD", 4, 0, Coordinates::VERTICAL))
   end
+
+  def test_can_check_coordinate_list_for_word
+    assert(@search.word_matches_coordinate_list?(
+      "EJOTY", [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4]]
+    ))
+    assert(@search.word_matches_coordinate_list?(
+      "PQRS", [[0, 3], [1, 3], [2, 3], [3, 3]]
+    ))
+    refute(@search.word_matches_coordinate_list?(
+      "WORD", [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4]]
+    ))
+  end
+
+
+
+
 end
