41553b945cfae3a1a7f411365e1cb5ad1e75bbdb
Charles Hoffman <chuck@hoff2.com>
2018-10-19 12:03:05 -0500

    test check whether word is at specific position & direction

--- lib/search.rb
+++ lib/search.rb
@@ -11,7 +11,7 @@ module WordSearch
 
     def word_at_position_in_direction?(word, column, row, direction)
       coordinates = coordinate_list(column, row, direction, word.length)
-      checks = coordinates.zip(word.split)
+      checks = coordinates.zip(word.split(''))
       checks.all? do |coordinate, letter|
         @grid.letter_match_at?(letter, coordinate[0], coordinate[1])
       end
