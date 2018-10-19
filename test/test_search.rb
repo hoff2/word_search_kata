1368eb11debe4d7dfeed5ee4cc08fbbba27b4af6
Charles Hoffman <chuck@hoff2.com>
2018-10-19 11:27:24 -0500

    refactor: search test setup

--- test/test_search.rb
+++ test/test_search.rb
@@ -4,19 +4,8 @@ require './lib/search.rb'
 class TestSearch < MiniTest::Test
   include WordSearch
 
-  def test_set_up_a_word_search
-    @grid = Grid.new([
-      %w(A B C D E),
-      %w(F G H I J),
-      %w(K L M N O),
-      %w(P Q R S T),
-      %w(U V W X Y)
-    ])
-    Search.new(%w(EJOTY), @grid)
-  end
-
-  def test_can_tell_if_word_is_at_position_in_direction
-    @grid = Grid.new([
+  def setup
+    grid = Grid.new([
       %w(A B C D E),
       %w(F G H I J),
       %w(K L M N O),
@@ -24,6 +13,9 @@ class TestSearch < MiniTest::Test
       %w(U V W X Y)
     ])
     @search = Search.new(%w(EJOTY), @grid)
+  end
+
+  def test_can_tell_if_word_is_at_position_in_direction
     @search.word_at_position_in_direction?("EJOTY", 4, 0, Coordinates::VERTICAL)
   end
 end
