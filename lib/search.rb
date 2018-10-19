4df0372bbe50b407f7619268a3592af6402b4530
Charles Hoffman <chuck@hoff2.com>
2018-10-19 13:26:14 -0500

    won't need the old version of this now

--- lib/search.rb
+++ lib/search.rb
@@ -9,14 +9,6 @@ module WordSearch
       @grid = grid
     end
 
-    def word_at_position_in_direction?(word, column, row, direction)
-      coordinates = coordinate_list(column, row, direction, word.length)
-      checks = coordinates.zip(word.split(''))
-      checks.all? do |coordinate, letter|
-        @grid.letter_match_at?(letter, coordinate[0], coordinate[1])
-      end
-    end
-
     def word_matches_coordinate_list?(word, coordinates)
       checks = coordinates.zip(word.split(''))
       checks.all? do |coordinate, letter|
--- test/test_search.rb
+++ test/test_search.rb
@@ -15,18 +15,6 @@ class TestSearch < MiniTest::Test
     @search = Search.new(%w(EJOTY), grid)
   end
 
-  def test_can_tell_if_word_is_at_position_in_direction
-    assert(@search.word_at_position_in_direction?(
-      "EJOTY", 4, 0, Coordinates::VERTICAL))
-    assert(@search.word_at_position_in_direction?(
-      "PQRS", 0, 3, Coordinates::HORIZONTAL))
-  end
-
-  def test_can_tell_if_word_is_not_at_position_in_direction
-    refute(@search.word_at_position_in_direction?(
-      "WORD", 4, 0, Coordinates::VERTICAL))
-  end
-
   def test_can_check_coordinate_list_for_word
     assert(@search.word_matches_coordinate_list?(
       "EJOTY", [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4]]
@@ -38,8 +26,4 @@ class TestSearch < MiniTest::Test
       "WORD", [[4, 0], [4, 1], [4, 2], [4, 3], [4, 4]]
     ))
   end
-
-
-
-
 end
